14131c8008e7949091c56ce825d499f1aff2692b
Alexandre l'Heritier <1499145+AlexlHer@users.noreply.github.com>
2026-05-14 13:04:22 +0200

    Add license

--- src/Grille.java
+++ src/Grille.java
@@ -1,3 +1,9 @@
+//------------------------------------------------------------------------------
+// Copyright 2018-2026 Alexandre l'Heritier
+// See the top-level LICENSE file for details.
+// SPDX-License-Identifier: GPL-3.0-or-later
+//------------------------------------------------------------------------------
+
 /**
  * Représente l'ensemble des cases du plateau.
  *
--- src/Pion.java
+++ src/Pion.java
@@ -1,3 +1,9 @@
+//------------------------------------------------------------------------------
+// Copyright 2018-2026 Alexandre l'Heritier
+// See the top-level LICENSE file for details.
+// SPDX-License-Identifier: GPL-3.0-or-later
+//------------------------------------------------------------------------------
+
 /**
  * Représente un pion avec un son numéro unique composé du num du joueur et	du num du pion.
  *
